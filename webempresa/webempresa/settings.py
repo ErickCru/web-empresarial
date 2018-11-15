cbda6b2e57faba223c7fd307a806f772957820c3
unknown <cruzperick@gmail.com>
2018-11-15 00:46:04 -0500

    Modificacion para funcionar con mysql.

--- webempresa/webempresa/settings.py
+++ webempresa/webempresa/settings.py
@@ -83,9 +83,17 @@ WSGI_APPLICATION = 'webempresa.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'mysql_cymysql',
+        'NAME': 'empresa',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
